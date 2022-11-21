a561d4f57c231373da8851eb64d482957226be2d
nnda <96333146+nndda@users.noreply.github.com>
2022-11-22 06:57:50 +0800

    print() for debug purpose

--- Scene/SetItem_.gd
+++ Scene/SetItem_.gd
@@ -7,6 +7,10 @@ export(String) var raw_array = ""
 func _ready():
 # warning-ignore:return_value_discarded
 	connect( "mouse_entered", self, "on_hover" )
+	print("on _set_itm")
+	print(file_path)
+	print(file_name)
+	print(raw_array)
 #	connect( "mouse_exited", self, "on_hover", [false] )
 
 func on_hover(is_true = true):

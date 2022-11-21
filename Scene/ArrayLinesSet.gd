f07baffc020ea058a704139761022f11a54bbc12
nnda <96333146+nndda@users.noreply.github.com>
2022-11-22 06:56:02 +0800

    Add options for "color" and "speed"

--- Scene/ArrayLinesSet.gd
+++ Scene/ArrayLinesSet.gd
@@ -18,6 +18,9 @@ onready var actor_expression_option			= self.get_node("VBoxContainer/HBoxContain
 onready var lines							= self.get_node("VBoxContainer/HBoxContainer/VBoxContainer/Editor/LinesEditor/Lines")
 
 onready var using_quote_option				= self.get_node("VBoxContainer/HBoxContainer/VBoxContainer/Editor/SubOptions/UseQuote")
+onready var using_color_option				= self.get_node("VBoxContainer/HBoxContainer/VBoxContainer/Editor/SubOptions/Color")
+onready var using_speed_option				= self.get_node("VBoxContainer/HBoxContainer/VBoxContainer/Editor/SubOptions/Speed")
+
 
 #onready var move_node						= self.get_node("VBoxContainer/HBoxContainer/PanelContainer/RearrangeCtrl")
 onready var move_up							= self.get_node("VBoxContainer/HBoxContainer/PanelContainer/RearrangeCtrl/MoveUp")
@@ -72,6 +75,8 @@ func set_lines_var():
 
 
 func _process(_delta):
+
+
 #	if self.visible and glbl.dict_chr != null:
 	if self.visible and glbl.current_dict != null:
 		step_label.text = str( self.get_position_in_parent() )
@@ -103,18 +108,33 @@ func _process(_delta):
 
 				loading = false
 
-		delete.disabled = !self.get_parent().get_child_count() > 1
 
+
+		delete.disabled = !self.get_parent().get_child_count() > 1
 		move_up.disabled = !self.get_position_in_parent() != 0
 		move_down.disabled = !self.get_position_in_parent() + 1 != self.get_parent().get_child_count()
 
-		actor_expression_option.visible = glbl.current_set_use_expr
-		actor_portrait.visible = glbl.current_set_use_expr
+
+
+		actor_name_option.get_parent().visible = glbl.dict_cfg.use_portraits
+		actor_name_option.get_parent().get_parent().get_node("HS2").visible = glbl.dict_cfg.use_portraits
+
+		using_color_option.disabled = glbl.dict_cfg.use_custom_color
+		using_color_option.visible = glbl.dict_cfg.use_custom_color
+
+		using_speed_option.disabled = glbl.dict_cfg.use_custom_speed
+		using_speed_option.visible = glbl.dict_cfg.use_custom_speed
+
+
+
+#		actor_expression_option.visible = glbl.current_set_use_expr
+#		actor_portrait.visible = glbl.current_set_use_expr
+#		actor_expression_option.visible = glbl.current_set_use_expr
+#		actor_portrait.visible = glbl.current_set_use_expr
 
 
 
 func refresh_data():
-#	actor_portrait.texture = glbl.current_portrait
 	actor_portrait.texture.set_atlas( glbl.current_portrait )
 	portrait_fit_scale()
 	if !glbl.dict_chr.empty():
@@ -133,36 +153,38 @@ func refresh_data():
 
 		self._on_Name_item_selected( actor_name_option.get_item_index( 0 ) )
 		self.data_set = true
-	print( glbl.portrait_crop( 0, 0 ) )
+#	print( glbl.portrait_crop( 0, 0 ) )
 
 
 
 func _on_Name_item_selected(index):
 	if glbl.dict_chr != null:
-		if !glbl.dict_chr.empty() and glbl.current_set_use_expr:
+		if !glbl.dict_chr.empty():
 
-			self.expressions_id.clear()
-			actor_expression_option.clear()
+			if glbl.dict_cfg.use_portraits:
 
-			for expression in (
-				glbl.dict_chr[ actor_name_option.get_item_text( index ) ][
-					"expressions"
-					].keys().size()
-				):
+				self.expressions_id.clear()
+				actor_expression_option.clear()
 
-					actor_expression_option.add_item(
-						glbl.dict_chr[
-							actor_name_option.get_item_text( index )
-							]["expressions"].keys()[ expression ],
-							expression
-						)
+				for expression in (
+					glbl.dict_chr[ actor_name_option.get_item_text( index ) ][
+						"expressions"
+						].keys().size()
+					):
 
-					self.expressions_id[str(glbl.dict_chr[
-							actor_name_option.get_item_text( index )
-							]["expressions"].keys()[ expression ])] = expression
+						actor_expression_option.add_item(
+							glbl.dict_chr[
+								actor_name_option.get_item_text( index )
+								]["expressions"].keys()[ expression ],
+								expression
+							)
 
-#			emit_signal("_on_Expression_item_selected")
-#			set_portrait_img()
+						self.expressions_id[str(glbl.dict_chr[
+								actor_name_option.get_item_text( index )
+								]["expressions"].keys()[ expression ])] = expression
+
+	#			emit_signal("_on_Expression_item_selected")
+	#			set_portrait_img()
 
 
 func move_up_set():
